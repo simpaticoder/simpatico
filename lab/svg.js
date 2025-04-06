e38f9264eac40f78f995b56a86e14b7060869ee0
simpaticoder <hello@simpatico.io>
2025-04-05 23:56:40 -0400

    svg: move the elt function back into the lib

--- lab/svg.js
+++ lab/svg.js
@@ -1,11 +1,3 @@
 const xpath = (strExpr, doc=d) => doc.evaluate(strExpr, doc, null, XPathResult.ANY_TYPE, null);
 
 const $ = document.querySelectorAll.bind(document);
-const elt = (idOrClass, parent) => {
-    if (parent){
-        return parent.getElementsByClassName(idOrClass)[0];
-    } else {
-        return document.getElementById(idOrClass);
-    }
-
-}
